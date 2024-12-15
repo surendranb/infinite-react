1428649fb6dda31db813b05d82d96a9ff97523e3
Surendran <reachsuren@gmail.com>
2024-12-15 11:00:15 +0530

    testing another fix

---
+++ src/app.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { AuthContext, AuthProvider } from './context/AuthContext';
+import Auth from './components/Auth';
+import MainApp from './MainApp';
+import Loading from './components/Loading';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+function App() {
+    const { currentUser, loading } = useContext(AuthContext);
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    return (
+        <div className="min-h-screen bg-gray-100">
+            {currentUser ? <MainApp /> : <Auth />}
+        </div>
+    );
+}
+
+const router = createBrowserRouter([
+   {
+     path: "/",
+     element: <App />
+   }
+]);
+
+function AppWrapper() {
+    return (
+        <AuthProvider>
+           <RouterProvider router={router} />
+        </AuthProvider>
+    )
+}
+
+export default AppWrapper;
