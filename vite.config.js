fea575f35574852ee1e2a1fd5497091641dbeb99
Surendran <reachsuren@gmail.com>
2024-12-15 11:03:22 +0530

    Enhance Vite configuration to specify Rollup input for the build process

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx',
+      },
+    },
+  },
 })
