1b1e6d31c37cdae8977d71e2d13a6fec9f93c2de
Surendran <reachsuren@gmail.com>
2024-12-21 17:38:36 +0530

    Refactor Vite configuration to restore public directory specification and streamline build options for improved asset management

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
    build: {
       external: ['fsevents'],
-       rollupOptions: {
-            input: {
-                main: 'src/main.jsx',
-            },
-       }
     },
+    publicDir: 'public',
 })
