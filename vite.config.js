906605df0607bf959b22258956f0c5c5311287db
Surendran <reachsuren@gmail.com>
2024-12-21 15:44:55 +0530

    Update Vite configuration to set main entry point to index.html and include path resolution for improved build process

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
    build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',
-      },
-       external: ['fsevents']
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'index.html'),
+            },
+        },
+      external: ['fsevents'],
     },
-  },
 })
