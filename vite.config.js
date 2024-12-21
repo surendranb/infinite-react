5a2aaca677bfec721ca3d01f09d0d17423fd0e80
Surendran <reachsuren@gmail.com>
2024-12-21 17:26:43 +0530

    Update Vite configuration to specify public directory and streamline rollup options for improved build process

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
    build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx',
-      },
-       external: ['fsevents']
+      external: ['fsevents'],
+       rollupOptions: {
+            input: {
+                main: 'src/main.jsx',
+            },
+       }
     },
-  },
 })
