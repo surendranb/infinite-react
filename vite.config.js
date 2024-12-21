6af1a786a27bd5577b030152933ca102f6f2f932
Surendran <reachsuren@gmail.com>
2024-12-21 15:56:18 +0530

    Refactor Vite configuration to set main entry point to src/main.jsx and streamline rollup options for improved build process

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
    build: {
-        rollupOptions: {
-            input: {
-                main: path.resolve(__dirname, 'index.html'),
-            },
-        },
-      external: ['fsevents'],
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx',
+      },
+       external: ['fsevents']
     },
+  },
 })
